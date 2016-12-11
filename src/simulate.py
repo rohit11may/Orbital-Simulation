b0dcc87e38857434a2d6e95d66482e41c69d30ba
Rohit <rohit11may@gmail.com>
2016-12-11 21:31:11 +0000

    Commented simulate.py

--- src/simulate.py
+++ src/simulate.py
@@ -22,16 +22,15 @@ Ui_MainWindow, QMainWindow = loadUiType('..//GUI//window.ui')
 
 def generate(x, y):
     for idx, n in enumerate(y):
-        y[idx] = randint(0, 10)
+        y[idx] = randint(0, 10) # Generates a random number from 1 to 10.
         x[idx] = idx
 
-
 def animate(i):
-    global x
+    global x #Use global to avoid the use of 'x' in the namespace of animate()
     global y
     global pos
     ax1.clear()
-    ax1.plot(x[0:pos], y[0:pos])
+    ax1.plot(x[0:pos], y[0:pos]) #Plot only up to a certain point of the arrays.
     pos += 1
 
 class Main(QMainWindow, Ui_MainWindow):
@@ -39,10 +38,10 @@ class Main(QMainWindow, Ui_MainWindow):
         super(Main, self).__init__()
         self.setupUi(self)
 
-    def addmpl(self, fig):
-        self.canvas = FigureCanvas(fig)
-        self.mplvl.addWidget(self.canvas)
-        self.canvas.draw()
+    def addMpl(self, fig):
+        self.canvas = FigureCanvas(fig) # Create canvas for figure.
+        self.mplvl.addWidget(self.canvas) # Add canvas as widget to mplvl
+        self.canvas.draw() # Draw canvas
 
 
 if __name__ == "__main__":
@@ -52,15 +51,16 @@ if __name__ == "__main__":
 
     fig = Figure()
     ax1 = fig.add_subplot(1, 1, 1)
-    main.addmpl(fig)
-    main.show()
+    main.addMpl(fig) #Add figure to the GUI
+    main.show() #Show GUI
 
-    y = multiprocessing.Array('d', 1000)
+    y = multiprocessing.Array('d', 1000) # Created shared memory space.
     x = multiprocessing.Array('d', 1000)
     pos = 0
-    p = multiprocessing.Process(target=generate, args=(x, y,))
-    p.start()
 
-    ani = animation.FuncAnimation(fig, animate, interval=3)
+    p = multiprocessing.Process(target=generate, args=(x, y,))
+    p.start() #Spawn new process.
+
+    ani = animation.FuncAnimation(fig, animate, interval=3) # Create animation updating every 3ms.
 
     sys.exit(app.exec_())
