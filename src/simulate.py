83c13d084a124ab8e8c5ce1874c032e103ee9a60
Rohit <rohit11may@gmail.com>
2016-12-11 23:22:22 +0000

    More comments. Added toolbar to GUI. Organised imports.

--- src/simulate.py
+++ src/simulate.py
@@ -3,22 +3,26 @@
 # 2016-2017
 # Last changed on 11/12/2016
 
-
-import multiprocessing
-import matplotlib.animation as animation
-import matplotlib.pyplot as plt
-from random import randint
-import numpy as np
+# GUI Libraries.
+from PyQt4 import QtGui
 from PyQt4.uic import loadUiType
-from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt4agg import (
     FigureCanvasQTAgg as FigureCanvas,
     NavigationToolbar2QT as NavigationToolbar)
+
+# Multiprocessing module.
+import multiprocessing
+
+# Matplotlib
+import matplotlib.animation as animation
+from matplotlib.figure import Figure
+
+# Others
+from random import randint
 import sys
-from PyQt4 import QtGui
 
 
-Ui_MainWindow, QMainWindow = loadUiType('..//GUI//window.ui')
+Ui_MainWindow, QMainWindow = loadUiType('..//GUI//window.ui') # Load UI file from GUI folder.
 
 def generate(x, y):
     for idx, n in enumerate(y):
@@ -33,7 +37,7 @@ def animate(i):
     ax1.plot(x[0:pos], y[0:pos]) #Plot only up to a certain point of the arrays.
     pos += 1
 
-class Main(QMainWindow, Ui_MainWindow):
+class Main(QMainWindow, Ui_MainWindow): # Go to Form -> View Code in QTDesigner to see structure of GUI.
     def __init__(self, ):
         super(Main, self).__init__()
         self.setupUi(self)
@@ -43,6 +47,11 @@ class Main(QMainWindow, Ui_MainWindow):
         self.mplvl.addWidget(self.canvas) # Add canvas as widget to mplvl
         self.canvas.draw() # Draw canvas
 
+    def addToolBar(self):
+        self.toolbar = NavigationToolbar(self.canvas, self.mplwindow, coordinates=True) #Instantiate toolbar.
+        self.mplvl.addWidget(self.toolbar) #Add toolbar to layout.
+
+
 
 if __name__ == "__main__":
 
@@ -51,16 +60,18 @@ if __name__ == "__main__":
 
     fig = Figure()
     ax1 = fig.add_subplot(1, 1, 1)
-    main.addMpl(fig) #Add figure to the GUI
+    main.addMpl(fig) # Add figure to the GUI
+    main.addToolBar() # Add toolbar to the GUI
     main.show() #Show GUI
 
-    y = multiprocessing.Array('d', 1000) # Created shared memory space.
-    x = multiprocessing.Array('d', 1000)
+    y = multiprocessing.Array('d', 1000) # Created array in the shared memory space.
+    x = multiprocessing.Array('d', 1000) # Created array in the shared memory space.
     pos = 0
 
-    p = multiprocessing.Process(target=generate, args=(x, y,))
+    p = multiprocessing.Process(target=generate, args=(x, y,)) # Must have trailing comma after final argument.
     p.start() #Spawn new process.
 
     ani = animation.FuncAnimation(fig, animate, interval=3) # Create animation updating every 3ms.
 
+    #p.join()
     sys.exit(app.exec_())
