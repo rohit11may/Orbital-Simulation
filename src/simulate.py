831858ebb1e45902bd16995ec01fcd2f6b8d4528
Rohit <rohit11may@gmail.com>
2016-12-11 21:27:52 +0000

    Imported the GUI. Embedded matplotlib graph in the gui using the main class. Working animation in the gui.

--- src/simulate.py
+++ src/simulate.py
@@ -3,13 +3,22 @@
 # 2016-2017
 # Last changed on 11/12/2016
 
-# Shared Memory
+
 import multiprocessing
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 from random import randint
-import time
+import numpy as np
+from PyQt4.uic import loadUiType
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt4agg import (
+    FigureCanvasQTAgg as FigureCanvas,
+    NavigationToolbar2QT as NavigationToolbar)
+import sys
+from PyQt4 import QtGui
 
+
+Ui_MainWindow, QMainWindow = loadUiType('..//GUI//window.ui')
 
 def generate(x, y):
     for idx, n in enumerate(y):
@@ -25,17 +34,33 @@ def animate(i):
     ax1.plot(x[0:pos], y[0:pos])
     pos += 1
 
+class Main(QMainWindow, Ui_MainWindow):
+    def __init__(self, ):
+        super(Main, self).__init__()
+        self.setupUi(self)
+
+    def addmpl(self, fig):
+        self.canvas = FigureCanvas(fig)
+        self.mplvl.addWidget(self.canvas)
+        self.canvas.draw()
+
 
 if __name__ == "__main__":
-    fig = plt.figure()
+
+    app = QtGui.QApplication(sys.argv)
+    main = Main()
+
+    fig = Figure()
     ax1 = fig.add_subplot(1, 1, 1)
+    main.addmpl(fig)
+    main.show()
 
     y = multiprocessing.Array('d', 1000)
     x = multiprocessing.Array('d', 1000)
     pos = 0
-
     p = multiprocessing.Process(target=generate, args=(x, y,))
     p.start()
 
     ani = animation.FuncAnimation(fig, animate, interval=3)
-    plt.show()
+
+    sys.exit(app.exec_())
